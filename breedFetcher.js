0700dc746bcf31db8ebde3da91b0b8c118cb0b75
rjlmacfarlane <rjl.macfarlane@gmail.com>
2020-12-03 14:47:27 -0400

    Added M&C tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const fetchBreedDescription = function(breed, callback) {
-  const domain = `https://api.thecata22pi.com/v1/breeds/search?q=${breed}`;
+  const domain = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
   request(domain, (error, response, body) => {
     if (!error) {
       const data = JSON.parse(body);
